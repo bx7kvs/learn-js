734650c3008a35276250ed0ba66eedb02003571e
Nyarro <langer360@gmail.com>
2025-01-31 17:32:00 +0100

    getSelTag

--- Class1hw.js
+++ Class1hw.js
@@ -1,6 +1,7 @@
 function Tags() {
     const tags = ["firstTag", "secondTag", "thirdTag"];
     this.addTag = (newTag) => {
+        this.getSelectedTag = () => newTag;  //Добавить проверку на выбранный тэг
         if (tags.includes(newTag)) {
             console.error(`Тэг уже существует`);
         } else {
@@ -12,15 +13,17 @@ function Tags() {
     if (tags.length === 0) {
         console.log("Должен быть хотя бы 1 тэг");
     }
+
+
 }
 
-function Article(name, shortText, longText, author, date, tags) {
+function Article(name, shortText, longText, author, date) {
     this.name = name;
     this.shortText = shortText;
     this.longText = longText;
     this.author = author;
     this.date = date;
-    this.tags = tags;
+    this.tags = tags.getSelectedTag();
 }
 
 function Web() {
@@ -31,11 +34,9 @@ function Web() {
 const tags = new Tags();
 console.log(tags.getAllowedTags());
 tags.addTag("rer");
+console.log(tags.getSelectedTag());
 
+const myNewWeb = new Web();
+let firstArticle = new Article('My favorite website', 'someshorttext', 'somelongtext', 'Arthur', 23.02);
+console.log(firstArticle);
 
-
-
-
-// const myNewWeb = new Web();
-// let firstArticle = new Article('My favorite website', 'someshorttext', 'somelongtext', 'Arthur', 23.02);
-// console.log(firstArticle);
