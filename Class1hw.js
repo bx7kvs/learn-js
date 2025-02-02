8cf1fcfe7519802b8cf712f8f8028a531bd5f45d
Nyarro <langer360@gmail.com>
2025-02-02 14:59:34 +0100

    FinishedAllTags

--- Class1hw.js
+++ Class1hw.js
@@ -1,12 +1,35 @@
 function Tags() {
     const tags = ["firstTag", "secondTag", "thirdTag"];
     this.addTag = (newTag) => {
-        this.getSelectedTag = () => newTag;  //Добавить проверку на выбранный тэг
         if (tags.includes(newTag)) {
             console.error(`Тэг уже существует`);
         } else {
             tags.push(newTag);
             console.log(`Тэг добавлен:`, newTag);
+        }
+    }
+    const selTags = []
+    this.selectTag = (selTag) => {
+        if (tags.includes(selTag)) {
+            selTags.push(selTag);
+            console.log("Выбран тэг:", selTag);
+
+
+        } else {
+            console.error(`Тэга не существует`);
+        }
+
+    }
+    this.tryFunk = () => {
+        return selTags;
+    }
+
+
+    this.getSelectedTag = () => {
+        if (selTags.length === 0) {
+            return "Должен быть хотя бы 1 тэг";
+        } else {
+            return selTags;
         }
     }
     this.getAllowedTags = () => [...tags];
@@ -24,6 +47,7 @@ function Article(name, shortText, longText, author, date) {
     this.author = author;
     this.date = date;
     this.tags = tags.getSelectedTag();
+
 }
 
 function Web() {
@@ -32,11 +56,14 @@ function Web() {
 }
 
 const tags = new Tags();
-console.log(tags.getAllowedTags());
-tags.addTag("rer");
-console.log(tags.getSelectedTag());
+tags.addTag("firstTag");
+tags.selectTag('firstTag');
+tags.addTag("rer2");
+tags.selectTag('rer2');
+console.log("Выбранные тэги:" + tags.getSelectedTag());
+
 
 const myNewWeb = new Web();
-let firstArticle = new Article('My favorite website', 'someshorttext', 'somelongtext', 'Arthur', 23.02);
+let firstArticle = new Article('My favorite website', 'someshorttext', 'somelongtext', 'Arthur', '23.02');
 console.log(firstArticle);
 
